51211d2657d1ba48b8296cbd9d8da61813881960
alex <megamanok99@gmail.com>
2021-08-07 17:03:39 +0300

    23

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import 'macro-css';
-import {BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-   <Router>
-   <App />
-   </Router>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
